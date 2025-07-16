9e2e813797fdf1522299e736397d3dabe8ecce1a
alejopua <alejopua@gmail.com>
2025-07-16 18:15:48 -0500

    refactor: update App component layout and clean up unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BasicTypes } from './assets/TypeScript/BasicTypes'
+
+// import { BasicTypes } from './assets/TypeScript/BasicTypes'
+// import { ObjectLiterals } from './assets/TypeScript/ObjectLiterals'
+import { BasicFunctions } from './assets/TypeScript/BasicFunctions';
 
 export const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='flex justify-center items-center h-svh'>
+          <h1 className="text-4xl font-bold m-5">
+            Hello world!
+          </h1>
+
       </div>
-      <BasicTypes />
+      {/* <BasicTypes /> */}
+      {/* <ObjectLiterals */}
+      {/* <BasicFunctions /> */}
 
     </>
   )
