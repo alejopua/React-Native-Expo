8521cf26da368b435a75b1e59a7cf0da6890e8c3
alejopua <alejopua@gmail.com>
2025-07-16 23:34:17 -0500

    refactor: clean up App component structure and remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-
-
 // import { BasicTypes } from './assets/TypeScript/BasicTypes'
 // import { ObjectLiterals } from './assets/TypeScript/ObjectLiterals'
 // import { BasicFunctions } from './assets/TypeScript/BasicFunctions';
 
-import { Counter } from "./components/Counter"
+// import { Counter } from "./components/Counter";
+import { AuthProvider } from "./context/AuthContext";
+import { LoginPage } from "./components/LoginPage";
 
 export const App = () => {
-
   return (
-    <>
-      <div className='flex flex-col justify-center items-center h-svh'>
-          <h1 className="text-4xl font-bold m-5">
-            Hello world!
-          </h1>
+    <AuthProvider>
+      <div className="flex flex-col justify-center items-center h-svh">
+        <h1 className="text-4xl font-bold m-5">Hello world!</h1>
 
-          {/* <BasicTypes /> */}
-          {/* <ObjectLiterals */}
-          {/* <BasicFunctions /> */}
-          <Counter />
+        {/* <BasicTypes /> */}
+        {/* <ObjectLiterals */}
+        {/* <BasicFunctions /> */}
+        {/* <Counter /> */}
+        <LoginPage />
       </div>
-
-    </>
-  )
-}
-
+    </AuthProvider>
+  );
+};
