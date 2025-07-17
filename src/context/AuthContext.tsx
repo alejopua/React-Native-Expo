609dd2007aae1afdab7600b89c5cb986adaa7160
alejopua <alejopua@gmail.com>
2025-07-16 23:32:13 -0500

    feat: enhance AuthContext with AuthStatus enum and update AuthState structure

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,17 +1,45 @@
-import { createContext, type PropsWithChildren, useContext } from "react";
+import {
+  createContext,
+  type PropsWithChildren,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 
+enum AuthStatus {
+  "checking" = "checking",
+  "authenticated" = "authenticated",
+  "unauthenticated" = "unauthenticated",
+}
 interface AuthState {
-  hola: string;
+  status: AuthStatus;
+  token?: string;
+  user?: User;
+  isChecking: boolean;
+}
+
+interface User {
+  UserName: string;
+  email: string;
 }
 const AuthContext = createContext({} as AuthState);
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
+  const [status, setStatus] = useState(AuthStatus.checking);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setStatus(AuthStatus.unauthenticated);
+    }, 2000);
+  }, []);
+
   return (
     <AuthContext.Provider
       value={{
-        hola: "Mundo",
+        status: status,
+        isChecking: status === AuthStatus.checking,
       }}
     >
       {children}
