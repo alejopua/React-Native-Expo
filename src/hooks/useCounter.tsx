6eb17bb6f426c09fef74e3f399b2c9265bb28beb
alejopua <alejopua@gmail.com>
2025-07-16 19:30:02 -0500

    feat: implement custom useCounter hook for managing count state

---
+++ src/hooks/useCounter.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export const useCounter = () => {
+  const [count, setCount] = useState<number>(10);
+
+  const increaseBy = (value: number) => {
+  setCount(Math.max(count + value, 0));
+  }
+
+  return {
+    // Properties
+    count,
+
+    //Actions
+    increaseBy
+  }
+}
