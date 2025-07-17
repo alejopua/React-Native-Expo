e752070cc48923e7790a6a8c64cdb1678db6334e
alejopua <alejopua@gmail.com>
2025-07-16 23:03:08 -0500

    refactor: format code for better readability in Counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,21 +1,31 @@
 import { useCounter } from "../hooks/useCounter";
 
 export const Counter = () => {
-  const {count, increaseBy} = useCounter();
-  
+  const { count, increaseBy } = useCounter();
+
   return (
     <>
-        {/* Title */}
-        <h3 className="text-2xl">Counter: <small className="font-bold">{count}</small></h3>
-        <div>
-
+      {/* Title */}
+      <h3 className="text-2xl">
+        Counter: <small className="font-bold">{count}</small>
+      </h3>
+      <div>
         {/* Button left */}
-        <button className="bg-blue-600 px-4 py-1 rounded text-white hover:bg-blue-500 mr-2" onClick={() => increaseBy(-1)}>-1</button>
+        <button
+          className="bg-blue-600 px-4 py-1 rounded text-white hover:bg-blue-500 mr-2"
+          onClick={() => increaseBy(-1)}
+        >
+          -1
+        </button>
 
         {/* Button right */}
-        <button className="bg-blue-600 px-4 py-1 rounded text-white hover:bg-blue-500" onClick={() => increaseBy(+1)}>+1</button>
+        <button
+          className="bg-blue-600 px-4 py-1 rounded text-white hover:bg-blue-500"
+          onClick={() => increaseBy(+1)}
+        >
+          +1
+        </button>
       </div>
     </>
-  )
-}
-
+  );
+};
