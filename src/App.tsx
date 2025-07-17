fb3c758988aa035f448a79a223e485c78902d841
alejopua <alejopua@gmail.com>
2025-07-16 19:29:48 -0500

    refactor: improve layout structure in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import reactLogo from './assets/react.svg'
+
 
 // import { BasicTypes } from './assets/TypeScript/BasicTypes'
 // import { ObjectLiterals } from './assets/TypeScript/ObjectLiterals'
-import { BasicFunctions } from './assets/TypeScript/BasicFunctions';
+// import { BasicFunctions } from './assets/TypeScript/BasicFunctions';
+
+import { Counter } from "./components/Counter"
 
 export const App = () => {
 
   return (
     <>
-      <div className='flex justify-center items-center h-svh'>
+      <div className='flex flex-col justify-center items-center h-svh'>
           <h1 className="text-4xl font-bold m-5">
             Hello world!
           </h1>
 
+          {/* <BasicTypes /> */}
+          {/* <ObjectLiterals */}
+          {/* <BasicFunctions /> */}
+          <Counter />
       </div>
-      {/* <BasicTypes /> */}
-      {/* <ObjectLiterals */}
-      {/* <BasicFunctions /> */}
 
     </>
   )
