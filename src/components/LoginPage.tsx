4e05ab9dddaf2805f52b8f8e22ad08e5fa1933ef
alejopua <alejopua@gmail.com>
2025-07-18 00:26:40 -0500

    feat: enhance LoginPage to include user information and improve logout functionality

--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -1,21 +1,34 @@
 import { useAuthContext } from "../context/AuthContext";
 
 export const LoginPage = () => {
-  const { isChecking, isAuthenticated } = useAuthContext();
+  const {
+    isChecking,
+    isAuthenticated,
+    loginWithEmailAndPassword,
+    user,
+    logout,
+  } = useAuthContext();
 
   return (
     <>
       {isAuthenticated ? (
         <>
           <h3>Welcome</h3>
-          <button className="bg-blue-500 p-2 rounded-xl mt-2 text-white">
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+          <button
+            onClick={() => logout()}
+            className="bg-blue-500 p-2 rounded-xl mt-2 text-white"
+          >
             Logout
           </button>
         </>
       ) : (
         <>
           <h3>Hello</h3>
-          <button className="bg-blue-500 p-2 rounded-xl mt-2 text-white">
+          <button
+            onClick={() => loginWithEmailAndPassword("alejo@gmail.com", "123")}
+            className="bg-blue-500 p-2 rounded-xl mt-2 text-white"
+          >
             Login
           </button>
         </>
