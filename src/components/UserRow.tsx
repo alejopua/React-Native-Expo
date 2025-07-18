264cb405b64adfda2ac574aee0b67b4b817d9ea1
alejopua <alejopua@gmail.com>
2025-07-18 18:24:43 -0500

    feat: add UserRow component for displaying user information in a table

---
+++ src/components/UserRow.tsx
@@ -0,0 +1,17 @@
+export const UserRow = () => {
+  return (
+    <>
+      <tr>
+        <td className="p-2 align-middle whitespace-nowrap ">
+          <div className="capitalize">success</div>
+        </td>
+        <td className="p-2 align-middle whitespace-nowrap ">
+          <div className="lowercase">sarah23@example.com</div>
+        </td>
+        <td className="p-2 align-middle whitespace-nowrap">
+          <div className="text-right font-medium">$1,280.00</div>
+        </td>
+      </tr>
+    </>
+  );
+};
