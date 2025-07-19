d266f5638e379c5fdda8324ab83622b6efcae151
alejopua <alejopua@gmail.com>
2025-07-18 20:04:41 -0500

    feat: add pagination functionality to useUser hook with nextPage and prevPage methods

--- src/hooks/useUser.tsx
+++ src/hooks/useUser.tsx
@@ -1,15 +1,37 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import type { User } from "../interfaces/reqres.response";
 import { loadUserActions } from "../actions/load-users.actions";
 
 export const useUser = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const currentPageRef = useRef(1);
 
   useEffect(() => {
     loadUserActions(1).then(setUsers);
   }, []);
 
+  const nextPage = async () => {
+    currentPageRef.current++;
+    const users = await loadUserActions(currentPageRef.current);
+
+    if (users.length > 0) {
+      setUsers(users);
+    } else {
+      currentPageRef.current--;
+    }
+  };
+
+  const prevPage = async () => {
+    if (currentPageRef.current < 1) return;
+
+    currentPageRef.current--;
+    const users = await loadUserActions(currentPageRef.current);
+    setUsers(users);
+  };
+
   return {
     users,
+    nextPage,
+    prevPage,
   };
 };
